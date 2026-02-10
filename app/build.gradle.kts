4da3a89054dd48b12a6f25f025f6c3cf86b067b9
Freetime Maker <jamieachatz@gmail.com>
2026-02-10 12:44:34 +0100

    Updated FreetimeSDK Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
-    implementation("com.github.FreetimeMaker:FreetimeSDK:v1.0.4")
+    implementation("com.github.FreetimeMaker:FreetimeSDK:v1.0.5")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
